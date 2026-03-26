9ca66c72085a27bc398b0fdd26056f87acaacf65
Silver-Eun <h22sang@naver.com>
2026-03-26 17:49:23 +0900

    fix: Health Check API 에러 수정
    @ResponseBody 추가

--- src/main/java/com/petmily/controller/HomeController.java
+++ src/main/java/com/petmily/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.petmily.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -14,6 +15,7 @@ public class HomeController {
 
     // cloudtype 콜드 스타트 방지용 api
     @GetMapping("/api/health")
+    @ResponseBody
     public String healthCheck() {
 
         return "OK";
